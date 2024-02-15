509197cee9dbf5ac6893d7ab46789b94849d4c1b
Patt Höge <paty.hoge98@gmail.com>
2024-02-15 15:21:54 +0000

    Like Serializer Handling Duplicate Likes / Integrity Errors

--- likes/serializers.py
+++ likes/serializers.py
@@ -1,3 +1,4 @@
+from django.db import IntegrityError
 from rest_framework import serializers
 from .models import Like
 
@@ -15,3 +16,11 @@ class LikeSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'created_at', 'owner', 'post'
         ]
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicate'
+            })
