6d3fd0c03ae0c67e81c6db752833c53574f9d86f
Patt Höge <paty.hoge98@gmail.com>
2024-02-15 11:55:14 +0000

    Added comment list and comment detail generic views

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from pp5_drf_api.permissions import IsOwnerOrReadOnly
+from .models import Comment
+from .serializers import CommentSerializer, CommentDetailSerializer
 
-# Create your views here.
+
+# Class provided by DRF-API walkthrough.
+class CommentList(generics.ListCreateAPIView):
+    """
+    List all comments
+    Create a new comment if authenticated
+    Associate the current logged in user with the comment.
+    """
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Comment.objects.all()
+    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+# Class provided by DRF-API walkthrough.
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve a comment
+    update or delete a comment if owner
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = CommentDetailSerializer
+    queryset = Comment.objects.all()
