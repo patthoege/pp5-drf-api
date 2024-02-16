42792a26ba331148c75ebb7d179ee576b5008d67
Patt Höge <paty.hoge98@gmail.com>
2024-02-16 18:28:03 +0000

    Added JSON Web Tokens to the project

--- pp5_drf_api/settings.py
+++ pp5_drf_api/settings.py
@@ -25,6 +25,23 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Sessions and tokens in development
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [(
+        'rest_framework.authentication.SessionAuthentication'
+        if 'DEV' in os.environ
+        else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
+    )]
+}
+
+REST_USE_JWT = True
+JWT_AUTH_SECURE = True
+JWT_AUTH_COOKIE = 'my-app-auth'
+JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
+
+REST_AUTH_SERIALIZERS = {
+    'USER_DETAILS_SERIALIZER': 'pp5_drf_api.serializers.CurrentUserSerializer'
+}
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -35,7 +52,7 @@ SECRET_KEY = 'django-insecure-c32ec8tsq)s@8tk)a+7pf%no$w+323_9!^9e$7547=swd4s)vf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-patthoege-pp5drfapi-w61mlmaor9w.ws-eu108.gitpod.io']
+ALLOWED_HOSTS = ['8000-patthoege-pp5drfapi-9jaxdhejpo9.ws-eu108.gitpod.io']
 
 
 # Application definition
@@ -51,6 +68,13 @@ INSTALLED_APPS = [
     'cloudinary',
     'rest_framework',
     'django_filters',
+    'rest_framework.authtoken',
+    'dj_rest_auth',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'dj_rest_auth.registration',
     
     'profiles',
     'posts',
@@ -58,6 +82,8 @@ INSTALLED_APPS = [
     'likes',
     'followers',
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
