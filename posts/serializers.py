21f213d1fd6ad746000915c0f128d237a3c8d0c9
Patt Höge <paty.hoge98@gmail.com>
2024-02-16 12:25:31 +0000

    addiedfollowing_id field to profiles and added like_id to posts

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Post
+from likes.models import Like
 
 
 # Class provided by DRF-API walkthrough.
@@ -8,6 +9,7 @@ class PostSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    like_id = serializers.SerializerMethodField()
 
 
     def validate_image(self, value):
@@ -30,10 +32,20 @@ class PostSerializer(serializers.ModelSerializer):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_like_id(self, obj):
+            user = self.context['request'].user
+            if user.is_authenticated:
+                like = Like.objects.filter(
+                    owner = user, post = obj
+                ).first()
+                return like.id if like else None
+            return None
+
     class Meta:
         model = Post
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
-            'title', 'content', 'image','location', 'image_filter'
+            'title', 'content', 'image','location', 'image_filter',
+            'like_id'
         ]
