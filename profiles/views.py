8179829bae4f6f4fbd23d2b4280889a6717288c0
Patt Höge <paty.hoge98@gmail.com>
2024-02-16 12:39:29 +0000

    added post_count, following_count and followers_count filters to the Profile view

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics
+from django.db.models import Count
+from rest_framework import generics, filters
 from pp5_drf_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
@@ -10,8 +11,29 @@ class ProfileList(generics.ListAPIView):
     List all profiles
     No Create view (post method), as profile creation handled by django signals
     """
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
+
+    filter_backends = [
+        filters.OrderingFilter,
+    ]
+
+    filterset_fields = [
+        'owner__following__followed__profile',
+        'owner__followed__owner__profile',
+    ]
+
+    ordering_fields = [
+        'posts_count',
+        'followers_count',
+        'following_count',
+        'owner__following__created_on',
+        'owner__followed__created_on',
+    ]
 
 
 # Class provided by DRF-API walkthrough.
