2cf9c598479b9ebdb6bd212d92998eccab76e5d5
Patt Höge <paty.hoge98@gmail.com>
2024-02-21 14:08:22 +0000

    updated the event model and added EventList for events

--- events/views.py
+++ events/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, filters
+from pp5_drf_api.permissions import IsOwnerOrReadOnly
+from .models import Event
+from .serializers import EventSerializer
 
-# Create your views here.
+
+class EventList(generics.ListCreateAPIView):
+    """
+    Retrieve events from DB.
+    Create new events.
+    """
+    serializer_class = EventSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Event.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
