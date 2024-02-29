623c8659c71a83615657205d9404311f2304a5aa
Patt Höge <paty.hoge98@gmail.com>
2024-02-29 10:33:16 +0000

    added posts_counts, followers_counts and following_counts to the ProfileDetail view

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -31,5 +31,5 @@ class ProfileSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'name', 'bio',
             'content', 'image', 'is_owner', 'following_id',
-             'posts_count', 'followers_count', 'following_count'
+            'posts_count', 'followers_count', 'following_count'
         ]
--- profiles/views.py
+++ profiles/views.py
@@ -44,5 +44,8 @@ class ProfileDetail(generics.RetrieveUpdateAPIView):
     Retrieve or update Profile fields if Owner.
     """
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+    posts_count=Count('owner__post', distinct=True),
+    followers_count=Count('owner__followed', distinct=True),
+    following_count=Count('owner__following', distinct=True)).order_by('-created_at')
     serializer_class = ProfileSerializer
