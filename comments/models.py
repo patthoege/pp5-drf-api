f9225399336e3838acca01c6c61ae58bc379fa4d
Patt Höge <paty.hoge98@gmail.com>
2024-03-05 12:19:42 +0000

    added a blank value to post field of comments model

--- comments/models.py
+++ comments/models.py
@@ -10,7 +10,7 @@ class Comment(models.Model):
     Comment model, related to User, Post and Event
     """
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True, blank=True)
     event = models.ForeignKey(Event, on_delete=models.CASCADE, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
